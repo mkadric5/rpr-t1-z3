53351dde1fb2a78c4504f94de47fa8e32e02ceea
mkadric5 <mkadric5@etf.unsa.ba>
2020-10-27 16:39:22 +0000

    Dodano tijelo main funkcije

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -4,5 +4,13 @@ public class Main{
 
     public static void main (String[] args)
     {
+        Sat s = new Sat(15,30,45);
+        System.out.println(s);
+        s.sljedeci();
+        System.out.println(s);
+        s.pomjeriZa(-48);
+        System.out.println(s);
+        s.postavi(0,0,0);
+        System.out.println(s);
     }
 }
